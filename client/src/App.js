84ca69e30199419bbf05a186f457122391137f7c
Davvos11 <vosdavid2@gmail.com>
2020-12-22 15:29:42 +0100

    setup a basic react / bootstrap app

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React, { Component } from 'react';
+import {Button, Container} from "react-bootstrap";
+
+class App extends Component {
+    render() {
+        return <Container>
+            <Button>Test</Button>
+        </Container>
+    }
+}
+
+export default App;
